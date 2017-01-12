d11febeb2fc9e405805f92c8d7df93c0388eb41a
cwen0708 <cwen0708@gmail.com>
2017-01-12 17:39:43 +0800

    路徑修正

--- controllers/album.py
+++ controllers/album.py
@@ -25,14 +25,14 @@ class Album(Controller):
     def admin_list(self):
         return scaffold.list(self)
 
-    @route_with('/admin/album/detail/<album_name>')
+    @route_with('/admin/album/album/detail/<album_name>')
     def admin_detail(self, album_name):
         album = self.meta.Model.find_by_name(album_name)
         if album is not None:
             self.context["album_item"] = album
             self.context["admin_detail"] = album.all_photo()
 
-    @route_with('/admin/album/insert')
+    @route
     def admin_insert(self):
         album = self.params.get_ndb_record('album')
         if album is not None:
@@ -40,22 +40,10 @@ class Album(Controller):
             return self.json({'info': 'done'})
         return self.json({'info': 'error'})
 
-    @route_with('/admin/album/create')
-    def create(self):
+    @route
+    def admin_create(self):
         album = self.meta.Model()
         album.title = self.params.get_string('name', u'未命名相簿')
         album.put()
         return self.json({'info': 'done'})
     #
-    # @route_menu(list_name=u'backend', text=u'圖片', sort=9700, icon='files-o', group=u'檔案管理')
-    # @route_with('/admin/user_file/images_list')
-    # def admin_images_list(self):
-    #     self.meta.pagination_limit = 12
-    #     model = self.meta.Model
-    #
-    #     def photo_factory(self):
-    #         return model.query(
-    #             model.content_type.IN(['image/jpeg', 'image/jpg', 'image/png', 'image/gif'])).order(
-    #             -model.content_type, -model.created, model._key)
-    #     self.scaffold.query_factory = photo_factory
-    #     return scaffold.list(self)
