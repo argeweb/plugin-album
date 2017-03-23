71d29be2aec6e9f26b4fd9a8e233a6e46bd533df
cwen0708 <cwen0708@gmail.com>
2017-03-23 21:23:38 +0800

    目標修正、欄位名稱變更

--- controllers/album.py
+++ controllers/album.py
@@ -16,7 +16,7 @@ import json
 class Album(Controller):
     class Meta:
         components = (scaffold.Scaffolding, Pagination, Search)
-        pagination_limit = 10
+        pagination_limit = 50
 
     class Scaffold:
         display_in_list = ('name', 'image', 'is_enable', 'category')
